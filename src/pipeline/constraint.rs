92616bf621a5e606f8a12953871f89c3d0952414
MarsMan13 <choigeon0513@gmail.com>
2025-06-14 10:37:41 +0000

    [Feat] Add constraint enum

---
+++ src/pipeline/constraint.rs
@@ -0,0 +1,20 @@
+use serde::{Deserialize, Serialize};
+use crate::pipeline::relation as rel;
+
+#[derive(Clone, Debug, Serialize, Deserialize)]
+#[serde(tag = "kind", rename_all = "camelCase")]
+pub enum Constraint {
+    RelEq { r1: rel::Relation, r2: rel::Relation },
+    AttrsEq { a1: rel::Expr, a2: rel::Expr },
+    PredEq { p1: rel::Expr, p2: rel::Expr },
+    SubAttr { a1: rel::Expr, a2: rel::Expr },
+    RefAttr {
+        r1: rel::Relation, a1: rel::Expr,
+        r2: rel::Relation, a2: rel::Expr
+    },
+    Unique { r: rel::Relation, a: rel::Expr },
+    NotNull { r: rel::Relation, a: rel::Expr },
+    FD      { r: rel::Relation, x: Vec<rel::Expr>, y: Vec<rel::Expr> },
+    Const   { r: rel::Relation, a: rel::Expr, c: rel::Expr },
+    Subset  { r1: rel::Relation, r2: rel::Relation },
+}
